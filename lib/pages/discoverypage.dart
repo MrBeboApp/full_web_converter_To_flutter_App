8c4a8d42e6da6ccc4c25041feb56d020a001ed79
Bahaa Ehab El Dien Abdelmawla <mbcompany888@gmail.com>
2020-08-23 15:40:45 +0200

    start edit Home Page

--- lib/pages/discoverypage.dart
+++ lib/pages/discoverypage.dart
@@ -5,49 +5,101 @@ class DiscoveryPage extends StatefulWidget {
   @override
   _DiscoveryPageState createState() => _DiscoveryPageState();
 }
-class _DiscoveryPageState extends State<DiscoveryPage> {
+class _DiscoveryPageState extends State<DiscoveryPage> with SingleTickerProviderStateMixin  {
+  TabController _controller;
 
-  List favItem = [1,4,5];
+  @override
+  void initState() {
+    super.initState();
+    _controller =  TabController(length: 2, vsync: this);
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ToolsUtilities.whiteColor,
-      body: Stack(
-        children: <Widget>[
+    return SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
 
-          Stack(
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height * 0.30,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: ToolsUtilities.mainColor,
-                  image: DecorationImage(image: NetworkImage(ToolsUtilities.discoveryImageHeader),fit: BoxFit.cover),
+            Stack(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.30,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: ToolsUtilities.mainColor,
+                    image: DecorationImage(image: NetworkImage(ToolsUtilities.discoveryImageHeader),fit: BoxFit.cover),
 
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 00.0, right: 10, left: 10),
+                  ),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 00.0, right: 10, left: 10),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.30,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: ToolsUtilities.secondColor.withOpacity(0.4),
+
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 00.0, right: 10, left: 10),
+                    child:_customHeading(),
+                  ),
+                ),
+              ],
+            ),
+
+
+            Padding(
+              padding: const EdgeInsets.only(right:20.0,left:20),
+              child: Container(
+                color: ToolsUtilities.whiteColor,
+                child: SizedBox(
+                  height: 2,
                 ),
               ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.30,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: ToolsUtilities.secondColor.withOpacity(0.4),
+            ),
+            Container(
+              decoration:  BoxDecoration(color: ToolsUtilities.mainColor),
 
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 00.0, right: 10, left: 10),
-                  child:_customHeading(),
-                ),
+              child:  TabBar(
+                indicatorColor: ToolsUtilities.whiteColor,
+                controller: _controller,
+                tabs: [
+                  Tab(
+                    icon: const Icon(Icons.lightbulb_outline),
+                    text: 'Top New',
+                  ),
+                  Tab(
+                    icon: const Icon(Icons.video_call),
+                    text: 'Top Videos',
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            Container(
+              color: ToolsUtilities.mainColor,
+              height: MediaQuery.of(context).size.height * 0.5,
+              child: new TabBarView(
+                controller: _controller,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SingleChildScrollView(
+                      child: Text("Home page")
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SingleChildScrollView(
+                      child: Text("youtube Channel"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
 
-
-
-        ],
-      ),
+          ],
+        ),
     );
   }
 
